7c594266c0fa384f00fdd2cf5ce26d755a21f931
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-17 00:30:43 +0200

    refactor

---
+++ ui_tools.py
@@ -0,0 +1,53 @@
+import sounddevice as sd
+import soundfile as sf
+import time
+import os
+
+def play(path):
+    data, fs = sf.read(path)
+    sd.play(data, fs)
+    status = sd.wait()
+    return status
+
+def check_successful(path):
+    _input = input("ok? [y/n] p for replay").lower()
+
+    success = True
+    if _input == "n":
+        success = False
+    elif _input == "p":
+        play(path)
+        check_successful(path)
+    elif _input != "y":
+        print("invalid entry")
+        success = check_successful(path)
+    return success
+
+def check_repeat(transcription):
+    _input = input("repeat? [y/n]").lower()
+    if _input == "y":
+        repeat = True
+    elif _input == "n":
+        repeat = False
+    else:
+        print("invalid entry")
+        check_repeat(transcription)
+    return repeat
+
+def check_finished(path, transcription, func):
+    play(path)
+    success = check_successful(path)
+    finished = True
+    if not success:
+        os.remove(path)
+        repeat = check_repeat(transcription)
+        if repeat:
+            finished = func(transcription)
+        else:
+            finished = True
+    return finished, success
+
+def show_transcription(transcription, go_signal):
+    print(transcription)
+    time.sleep(1)
+    go_signal()
--- voices_sampler.py
+++ voices_sampler.py
@@ -1,100 +1,51 @@
-import librosa
-
 import sounddevice as sd
-import soundfile as sf
 from scipy.io.wavfile import write
+import ui_tools as ui
 import time
-import os
 import numpy as np
 import pandas as pd
+
 
 class VoiceSampler:
     def __init__(self, fs=44100, seconds=3, file_path="files/", go_signal_path="go.wav", device='digital output'):
         self.fs = fs
         self.seconds = seconds
         self.file_path = file_path
-        self.go_signal_path = file_path+go_signal_path
+        self.go_signal_path = file_path + go_signal_path
         self.device = device
-        self.transcriptions = []
+        self.transctripts = []
 
     def go_signal(self):
-        self.play(self.go_signal_path)
-
-    def play(self, path):
-        data, fs = sf.read(path)
-        sd.play(data, fs)
-        status = sd.wait()
-        return status
-
-    def check_successful(self, path):
-        _input = input("ok? [y/n] p for replay").lower()
-
-        success = True
-        if _input == "n":
-            success = False
-        elif _input == "p":
-            self.play(path)
-            self.check_successful(path)
-        elif _input != "y":
-            print("invalid entry")
-            success = self.check_successful(path)
-        return success
-
-    def check_repeat(self, transcription):
-        _input = input("repeat? [y/n]").lower()
-        if _input == "y":
-            repeat = True
-        elif _input == "n":
-            repeat = False
-        else:
-            print("invalid entry")
-            self.check_repeat(transcription)
-        return repeat
-
-    def check_finished(self, path, transcription):
-        self.play(path)
-        success = self.check_successful(path)
-        finished = True
-        if not success:
-            os.remove(path)
-            repeat = self.check_repeat(transcription)
-            if repeat:
-                finished = self.sample(transcription)
-            else:
-                finished = True
-        return finished, success
+        ui.play(self.go_signal_path)
 
     def record(self, path):
         recording = sd.rec(int(self.seconds * self.fs), samplerate=self.fs, channels=2)
         sd.wait()
         write(path, self.fs, recording)
 
-    def show_transcription(self, transcription):
-        print(transcription)
-        time.sleep(1)
-        self.go_signal()
-
     def make_transcript_entry(self, id=0, transcription="", normalized_transcription=""):
-        self.transcriptions.append([id,transcription,normalized_transcription])
+        self.transctripts.append([id, transcription, normalized_transcription])
 
     def save_tramscript(self, path):
-        df = pd.DataFrame(np.asarray(self.transcriptions),columns=["id","transcription","normalized_transcription"])
+        df = pd.DataFrame(np.asarray(self.transctripts), columns=["id", "transcription", "normalized_transcription"])
         df.to_csv(path, sep="\t")
 
-    def sample(self, transcription="halllo ich bin lukas"):
+    def sample(self, transcription=""):
         id = str(time.time())
-        self.show_transcription(transcription)
-        path = self.file_path + id + ".wav"
+        ui.show_transcription(transcription, self.go_signal)
+        path = self.file_path + "samples/" + id + ".wav"
         self.record(path)
-        finished, success = self.check_finished(path, transcription)
+        finished, success = ui.check_finished(path, transcription, self.sample)
         if success:
             self.make_transcript_entry(id=id, transcription=transcription, normalized_transcription=transcription)
 
-    def make_dataset(self, target_transcriptions):
-        for transcription in target_transcriptions:
+    def make_dataset(self, transcriptions):
+        id = str(time.time())
+        for transcription in transcriptions:
             self.sample(transcription)
-        self.save_tramscript(self.file_path+"transcriptions.csv")
+        self.save_tramscript(self.file_path + "transcriptions/transcriptions_" + id + ".csv")
 
-transcription = ["A","B", "C"]
+
+transcription = ["A", "B", "C"]
 v = VoiceSampler(seconds=1)
 v.make_dataset(transcription)
