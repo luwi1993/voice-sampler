f85c62d715f8d876be27830af1877a9003b1dedd
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-17 14:08:16 +0200

    add conv filter

--- preprocess.py
+++ preprocess.py
@@ -4,9 +4,10 @@ import librosa
 from scipy.io.wavfile import write
 from ui_tools import play
 
+
 class VoicePreprocessor:
     def __init__(self, fs=22050):
-        self.fs  = fs
+        self.fs = fs
 
     def load(self, path):
         self.samples, self.sample_rate = librosa.load(path)
@@ -14,12 +15,25 @@ class VoicePreprocessor:
         self.n_samples = len(self.samples)
 
     def plot(self):
-        plt.plot(np.arange(self.n_samples)/self.sample_rate ,self.samples)
+        plt.plot(np.arange(self.n_samples) / self.sample_rate, self.samples)
         plt.show()
 
-    def filter(self, threshold = 0.0005):
-        above_threshold = threshold < np.abs(self.samples)
-        self.samples = self.samples[above_threshold]
+    def single_filter(self, threshold=0.0005):
+        return threshold < np.abs(self.samples)
+
+    def conv_filter(self, threshold=0.0005, window_size=100):
+        c = np.convolve(np.abs(self.samples), [1/window_size for _ in range(window_size)], 'same')
+        return threshold < c
+
+    def get_filter(self, mode="single", threshold=0.001):
+        if mode == "single":
+            filter = self.single_filter(threshold)
+        elif mode == "conv":
+            filter = self.conv_filter(threshold)
+        return filter
+
+    def filter(self, mode="conv"):
+        self.samples = self.samples[self.get_filter(mode=mode)]
         self.n_samples = len(self.samples)
 
     def write(self, path):
