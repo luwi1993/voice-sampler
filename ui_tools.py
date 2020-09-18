b32b1a6f94cd6049d4317681ae0599c44256b8fc
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-18 15:57:28 +0200

    fix

--- ui_tools.py
+++ ui_tools.py
@@ -17,7 +17,7 @@ def check_successful(path):
         success = False
     elif _input == "p":
         play(path)
-        check_successful(path)
+        success = check_successful(path)
     elif _input != "y":
         print("invalid entry")
         success = check_successful(path)
@@ -39,11 +39,11 @@ def check_finished(path, transcription, func):
     success = check_successful(path)
     finished = True
     if not success:
+        os.remove(path)
         repeat = check_repeat(transcription)
         if repeat:
             finished = func(transcription)
         else:
-            os.remove(path)
             finished = True
     return finished, success
 
